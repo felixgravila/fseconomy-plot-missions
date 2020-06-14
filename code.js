7b8127238867a07a0b1d06f5384490a99a07eca2
Felix Gravila <felixgravila@gmail.com>
2020-06-14 14:52:11 +0000

    Add more planes

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 
-const planes = ['Boeing 737-800', 'Boeing 747-400', 'Airbus A320', 'Airbus A321', 'Boeing 727-100/200']
+const planes = ['Boeing 737-800', 'Boeing 747-400', 'Airbus A320', 'Airbus A321', 'Boeing 727-100/200', 'Douglas DC-8 (10-40)', 'Douglas DC-6', 'McDonnell Douglas DC-10-30F']
+
 planes.sort()
 for(idx in planes){
     $('#plane-select').append(`<option value="${idx}">${planes[idx]}</option>`)
