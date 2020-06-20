f792d426a1fdc9a2f90ad13252831505a3acdecd
Felix Gravila <felixgravila@gmail.com>
2020-06-20 11:23:23 +0200

    Comment and refactor code

--- code.js
+++ code.js
@@ -13,8 +13,8 @@ const planes = [
     "McDonnell Douglas DC-10-30F"
 ]
 
+// create a easy to use map of possibly existing cookies
 function makeCookieMap(){
-    // create a easy to use map of possibly existing cookies
     let cookieMap = {};
     if ( document.cookie.length == 0 ) {
         return {}
@@ -28,15 +28,16 @@ function makeCookieMap(){
     return cookieMap;
 }
 
+// initialise the cookie map
 let cookieMap = makeCookieMap();
 
-
+// sort the plane list so they're always in alphabetical order
 planes.sort()
 for(idx in planes){
     $('#plane-select').append(`<option value="${idx}">${planes[idx]}</option>`)
 }
 
-// set plane if cookie
+// set last used plane in list if cookie
 if ( cookieMap["plane"] ) {
     $('#plane-select').val(cookieMap["plane"])
 }
@@ -52,6 +53,7 @@ if ( cookieMap["saveCookieTick"] ) {
 }
 
 // warn on remove tick that cookies will be deleted
+// only if cookies already exist
 function tickBoxTicked(){
     if ( cookieMap["saveCookieTick"] && !$("#saveDataTick").is(":checked") ) {
         alert("All cookies will be removed on next plot if box is left unticked.")
@@ -69,6 +71,8 @@ let on_map = [];
 let access;
 const icaodata = new Map();
 
+// load the icao data and form dictionary
+// info needed for icao lat and lon
 $.ajax({
     url: 'icaodata_small.csv',
     type: 'GET',
@@ -82,25 +86,98 @@ $.ajax({
     error: handle_ajax_err
 });
 
-
+// proxy is needed for CORS
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
+// function that logs ajax errors
 function handle_ajax_err(xhr, status){
     console.log(xhr);
     console.log(status);
 }
 
-function handle_planes_get(data){
-    const result = $.csv.toArrays(data); // create array from csv
-    const header = result[0]
-    const planes = result.slice(1) // remove header
+// main function called on click of plot button
+// gets information, plots on map, opens map
+function plot(){
 
+    // get information from form
+    const planeIdInList = $('#plane-select').val();
+    const plane = planes[planeIdInList];
+    access = sanitizeString($('#input-access-key').val());
+    const saveTheCookie = $("#saveDataTick").is(":checked")
+
+    // handle cookies if tickbox ticked
+    // or clear cookies
+    if ( saveTheCookie ){
+        document.cookie = `plane=${planeIdInList}`;
+        document.cookie = `accesskey=${access}`;
+        document.cookie = 'saveCookieTick=true';
+        cookieMap = makeCookieMap()
+    } else {
+        document.cookie = `plane=`;
+        document.cookie = `accesskey=`;
+        document.cookie = 'saveCookieTick=';
+        cookieMap = makeCookieMap()
+    }
+
+    // verify access key length is 10 (old version) or 16
+    // otherwise obviously bad key, exit
+    if ( access.length != 10 && access.length != 16 && !mock ) {
+        alert("Bad access token")
+        return
+    }
+
+    // define URL to get all planes
+    const plane_get_url = encodeURI(`https://server.fseconomy.net/data?userkey=${access}&format=csv&query=aircraft&search=makemodel&makemodel=${plane}`)
+    console.log(`getting ${plane_get_url}`);
+
+    // initialise the map
+    if(!map_initialised){
+        initialise_map();
+    }
+
+    // clear the map
+    for (l of on_map){
+        the_map.removeLayer(l)
+    }
+    on_map = []
+    
+    // execute ajax request to retrieve planes
+    if (mock) {
+        $.ajax({
+            url: 'test_data/aircraft.csv',
+            type: 'GET',
+            success: handle_planes_get,
+            error: handle_ajax_err
+        });
+    } else {
+        $.ajax({
+            url: proxy+plane_get_url,
+            type: 'GET',
+            success: handle_planes_get,
+            error: handle_ajax_err
+        });
+    }
+}
+
+// function executed after plane request returns
+// gets ICAOs and requests all their missions
+function handle_planes_get(data){
+    // parse result
+    const result = $.csv.toArrays(data); // create array from csv
+    const header = result[0]; // save header separately
+    const planes = result.slice(1); // remove header from data
+
+    // stop if short result
     if ( planes.length <= 1 ) {
         alert("Something went wrong, possibly wrong access key. Please make sure it's correct.")
         return
     }
 
     console.log(`Got ${planes.length} planes`)
+
+    // save planeIds to be used when finding which missions are relevant
+    // save locations (icaos) to be used when requesting missions
+    // iterate through planes to get info
     planeIds = []
     locations = []
     for (p of planes){
@@ -116,11 +193,13 @@ function handle_planes_get(data){
 
     console.log(`${locations.length} unique ICAOs`)
 
+    // form URL to get missions
     const joined_locs = locations.join("-")
-    
     icaos_url = encodeURI(`https://server.fseconomy.net/data?userkey=${access}&format=csv&query=icao&search=jobsfrom&icaos=${joined_locs}`)    
     console.log(`getting ${icaos_url}`);
     
+    // execute request
+    // use closure to pass planeIds to handle_missions_get as opposed to global variables
     if (mock){
         $.ajax({
             url: 'test_data/icao.csv',
@@ -138,6 +217,64 @@ function handle_planes_get(data){
     }
 }
 
+// function that handles the response containing missions
+function handle_missions_get(planeids){
+    return function(data){
+        // parse result
+        const result = $.csv.toArrays(data); // create array from csv
+        const header = result[0] // save header separately
+        const all_missions = result.slice(1); // remove header
+        console.log(`Got ${all_missions.length} total missions from the airports`)
+
+        // create list of missions with From and To ICAOs by iterating over the result
+        // and checking which missions have the AircraftId field in planeids
+        const missions = []
+        for ( m of all_missions ) {
+            let m_pid = parseInt(m[header.indexOf("AircraftId")])
+            if ( m_pid && planeids.indexOf(m_pid) >= 0 ) {
+                missions.push({"fromIcao": m[header.indexOf("FromIcao")], "toIcao": m[header.indexOf("ToIcao")]}) // FromICAO, ToICAO
+            }  
+        }
+
+        console.log(`${missions.length} relevant missions found`);
+
+        // plot the missions on the map
+        for (mission of missions) {
+            // get latlons from icaodata
+            from_latlon = icaodata[mission.fromIcao]
+            to_latlon = icaodata[mission.toIcao]
+            // green circle marker for From
+            const fromIcaoMarker = L.circleMarker(from_latlon, {
+                color: 'green',
+                radius: '5',
+                zIndex: 100
+            }).addTo(the_map);
+            // smaller red circle marker on top of the green one for To
+            const toIcaoMarker = L.circleMarker(to_latlon, {
+                color: 'red',
+                radius: '3',
+                zIndex: 101
+            }).addTo(the_map);
+            const lineBetween = L.polyline([from_latlon, to_latlon], {zIndex: 50}).addTo(the_map)
+
+            // add popup with ICAO on mouse hover
+            bindMarkerMessage(fromIcaoMarker, mission.fromIcao)
+            bindMarkerMessage(toIcaoMarker, mission.toIcao)
+
+            // Goto fseconomy airport on marker click
+            onClickGotoFseconomy(fromIcaoMarker, mission.fromIcao)
+            onClickGotoFseconomy(toIcaoMarker, mission.toIcao)
+
+            // save layers to list so they can be retrieved later
+            on_map.push(fromIcaoMarker)
+            on_map.push(toIcaoMarker)
+            on_map.push(lineBetween)
+
+        }
+    }
+}
+
+// function that binds the message to markers
 function bindMarkerMessage(marker, message){
     marker.bindPopup(message);
     marker.on('mouseover', function (e) {
@@ -148,126 +285,32 @@ function bindMarkerMessage(marker, message){
     });
 }
 
+// function that opens the fseconomy airport in a new tab on marker click
 function onClickGotoFseconomy(marker, icao) {
     marker.on('click', e => window.open(`https://server.fseconomy.net/airport.jsp?icao=${icao}`,'_blank'));
 }
 
-function handle_missions_get(planeids){
-    return function(data){
-        const result = $.csv.toArrays(data); // create array from csv
-        const header = result[0]
-        const all_missions = result.slice(1);
-        console.log(`Got ${all_missions.length} total missions from the airports`)
-
-        const missions = []
-        for ( m of all_missions ) {
-            let m_pid = parseInt(m[header.indexOf("AircraftId")])
-            if ( m_pid && planeids.indexOf(m_pid) >= 0 ) {
-                missions.push({"fromIcao": m[header.indexOf("FromIcao")], "toIcao": m[header.indexOf("ToIcao")]}) // FromICAO, ToICAO
-            }
-            
-        }
-
-        console.log(`${missions.length} relevant missions found`);
-
-        for (mission of missions) {
-            from_latlon = icaodata[mission.fromIcao]
-            to_latlon = icaodata[mission.toIcao]
-            const fromIcaoMarker = L.circleMarker(from_latlon, {
-                color: 'green',
-                radius: '3'
-            }).addTo(the_map);
-            const toIcaoMarker = L.circleMarker(to_latlon, {
-                color: 'red',
-                radius: '4'
-            }).addTo(the_map);
-            const lineBetween = L.polyline([from_latlon, to_latlon]).addTo(the_map)
-
-            bindMarkerMessage(fromIcaoMarker, mission.fromIcao)
-            bindMarkerMessage(toIcaoMarker, mission.toIcao)
-
-            onClickGotoFseconomy(fromIcaoMarker, mission.fromIcao)
-            onClickGotoFseconomy(toIcaoMarker, mission.toIcao)
-
-            on_map.push(fromIcaoMarker)
-            on_map.push(toIcaoMarker)
-            on_map.push(lineBetween)
-
-        }
-    }
-}
-
+// function that ensures the key only contains valid characters
 function sanitizeString(str){
     str = str.replace(/[^A-Z0-9]/g,"");
     return str.trim();
 }
 
-function plot(){
-
-    const planeIdInList = $('#plane-select').val();
-    const plane = planes[planeIdInList];
-    access = sanitizeString($('#input-access-key').val());
-    const saveTheCookie = $("#saveDataTick").is(":checked")
-
-    if ( saveTheCookie ){
-        document.cookie = `plane=${planeIdInList}`;
-        document.cookie = `accesskey=${access}`;
-        document.cookie = 'saveCookieTick=true';
-        cookieMap = makeCookieMap()
-    } else {
-        document.cookie = `plane=`;
-        document.cookie = `accesskey=`;
-        document.cookie = 'saveCookieTick=';
-        cookieMap = makeCookieMap()
-    }
-
-    if ( access.length != 10 && access.length != 16 && !mock ) {
-        alert("Bad access token")
-        return
-    }
-
-    const plane_get_url = encodeURI(`https://server.fseconomy.net/data?userkey=${access}&format=csv&query=aircraft&search=makemodel&makemodel=${plane}`)
-    console.log(`getting ${plane_get_url}`);
-
-    if(!map_initialised){
-        initialise_map();
-    }
-
-    for (l of on_map){
-        the_map.removeLayer(l)
-    }
-    on_map = []
-    
-    if (mock) {
-        $.ajax({
-            url: 'test_data/aircraft.csv',
-            type: 'GET',
-            success: handle_planes_get,
-            error: handle_ajax_err
-        });
-    } else {
-        $.ajax({
-            url: proxy+plane_get_url,
-            type: 'GET',
-            success: handle_planes_get,
-            error: handle_ajax_err
-        });
-    }
-
-
-}
-
-
-
+// function that adds relevant classes to the map jumbo for expansion
 function transitionOpen(){
     $("#map-jumbo").addClass("transition")
     $("#map-jumbo").addClass("expanded")
 }
 
+// function that initialises the map
 function initialise_map(){
+    // save flag so it only gets done once
     map_initialised = true;
+
+    // create and initialise map
     the_map = L.map('map').setView([20, 0], 3);
 
+    // add mapbox layer
     mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -278,35 +321,40 @@ function initialise_map(){
         zoomOffset: -1
     });
 
+    // add google hybrid layer
     googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3'],
         attribution: 'Map data ©2020 Google'
     });
 
+    // add google satellite layer
     googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3'],
         attribution: 'Map data ©2020 Google'
     });
 
+    // add google terrain layer
     googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3'],
         attribution: 'Map data ©2020 Google'
     });
 
+    // initialise using mapbox
     the_map.addLayer(mapbox);
 
+    // add all to dictionary for control 
     var baseMaps = {
         "Mapbox": mapbox,
         "Google Hybrid": googleHybrid,
         "Google Satellite": googleSat,
         "Google Terrain": googleTerrain
     };
-
     L.control.layers(baseMaps).addTo(the_map);
 
+    // timing trick for animation
     $("#map-jumbo").addClass("init");
     window.setTimeout(transitionOpen, 100);
 
